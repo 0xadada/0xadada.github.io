c9c3c191611f10a0b1220ab0a82c2bfca726ef75
0xADADA <0xadada.pub@0xadada.pub>
2022-01-27 22:15:33 -0500

    update deploy

--- pages/[year]/[month]/[day]/[slug].tsx
+++ pages/[year]/[month]/[day]/[slug].tsx
@@ -1,57 +1,59 @@
-import { GetStaticPaths, GetStaticProps, GetStaticPropsResult } from "next";
-import Head from "next/head";
-import Layout from "../../../../components/layout";
-import Metatags from "../../../../components/metatags";
-import DisplayDate from "../../../../components/display-date";
-import PostContent from "../../../../components/post-content";
-import Byline from "../../../../components/byline";
-import { getPostBySlug, getAllPosts } from "../../../../lib/api";
-import markdownToHtml from "../../../../lib/markdown-to-html";
-import { SITENAME, BASEURL } from "../../../../lib/constants";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsResult } from 'next';
+import Head from 'next/head';
+import Layout from '../../../../components/layout';
+import Metatags from '../../../../components/metatags';
+import DisplayDate from '../../../../components/display-date';
+import PostContent from '../../../../components/post-content';
+import Byline from '../../../../components/byline';
+import { getPostBySlug, getAllPosts } from '../../../../lib/api';
+import markdownToHtml from '../../../../lib/markdown-to-html';
+import License from '../../../../components/License';
+import { SITENAME, BASEURL } from '../../../../lib/constants';
 
 export async function getStaticProps({ params }) {
   const filename = `${params.year}-${params.month}-${params.day}-${params.slug}`;
   const post = getPostBySlug(filename, [
-    "title",
-    "displayTitle",
-    "metaDescription",
-    "metaKeywords",
-    "image",
-    "metaImage",
-    "date",
-    "author",
-    "content",
-    "slug",
-    "slugs"
+    'title',
+    'displayTitle',
+    'metaDescription',
+    'metaKeywords',
+    'image',
+    'metaImage',
+    'date',
+    'author',
+    'content',
+    'slug',
+    'slugs',
+    'license',
   ]);
-  const content = await markdownToHtml(post.content || "");
+  const content = await markdownToHtml(post.content || '');
 
   return {
     props: {
       post: {
         ...post,
-        content
-      }
-    }
+        content,
+      },
+    },
   };
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const posts = getAllPosts(["slug"]);
+  const posts = getAllPosts(['slug']);
 
-  const paths = posts.map(post => {
+  const paths = posts.map((post) => {
     return {
       params: {
         year: post.slugs.year,
         month: post.slugs.month,
         day: post.slugs.day,
-        slug: post.slugs.slug
-      }
+        slug: post.slugs.slug,
+      },
     };
   });
   return {
     paths,
-    fallback: false
+    fallback: false,
   };
 };
 
@@ -71,6 +73,7 @@ export default function Post({ post }) {
             <Byline author={post.author} />
           </header>
           <PostContent content={post.content} />
+          <License license={post.license || null} />
         </article>
       </Layout>
     </>
