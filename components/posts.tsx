a8d4fb9eb5d65b6eb176084bf6c3fc9024510cf7
0xADADA <0xadada.pub@0xadada.pub>
2022-03-27 18:03:33 -0400

    added index by year

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,11 +1,55 @@
-import Link from "next/link";
-import DisplayDate from "./display-date.tsx";
-import styles from "./posts.module.css";
+import Link from 'next/link';
+import DisplayDate from './display-date';
+import styles from './posts.module.css';
 
 export default function Posts({ posts }) {
+  const uniqYears: string[] = [];
+  posts.forEach(
+    (post) =>
+      !uniqYears.includes(post.slugs.year) && uniqYears.push(post.slugs.year)
+  );
+  const postsByYears = {};
+  uniqYears.forEach((year) => {
+    postsByYears[year] = posts.filter((post) => post.slugs.year === year);
+  });
+  return (
+    <ol className={styles.years}>
+      {uniqYears.map((year) => (
+        <li key={year} className={styles.yearItem}>
+          <span>{year}</span>
+          <ol className={styles.index}>
+            {postsByYears[year].map((post) => {
+              const year = post.slugs.year;
+              const month = post.slugs.month;
+              const day = post.slugs.day;
+              const slug = post.slugs.slug;
+
+              return (
+                <li key={post.title} className={styles.item}>
+                  <Link
+                    href={{
+                      pathname: '/[year]/[month]/[day]/[slug]',
+                      query: { year, month, day, slug },
+                    }}
+                  >
+                    {post.title}
+                  </Link>
+                  <br />
+                  <span>{post.metaDescription}</span>
+                  <br />
+                  <DisplayDate datetime={post.date} />
+                </li>
+              );
+            })}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+
   return (
     <ol className={styles.index}>
-      {posts.map(post => {
+      {posts.map((post) => {
         const year = post.slugs.year;
         const month = post.slugs.month;
         const day = post.slugs.day;
@@ -14,8 +58,8 @@ export default function Posts({ posts }) {
           <li key={post.title} className={styles.item}>
             <Link
               href={{
-                pathname: "/[year]/[month]/[day]/[slug]",
-                query: { year, month, day, slug }
+                pathname: '/[year]/[month]/[day]/[slug]',
+                query: { year, month, day, slug },
               }}
             >
               {post.title}
--- lib/api.ts
+++ lib/api.ts
@@ -1,21 +1,20 @@
-import fs from "fs";
-import { join } from "path";
-import matter from "gray-matter";
-import { format } from "date-fns";
+import fs from 'fs';
+import { join } from 'path';
+import matter from 'gray-matter';
 
-const postsDirectory = join(process.cwd(), "_posts");
+const postsDirectory = join(process.cwd(), '_posts');
 
 function getPostFilenames() {
   return fs.readdirSync(postsDirectory);
 }
 
-export function getPostBySlug(slug, fields = []) {
-  const realSlug = slug.replace(/\.md$/, "");
+export function getPostBySlug(slug: string, fields = []) {
+  const realSlug = slug.replace(/\.md$/, '');
   const year = realSlug.slice(0, 4);
   const month = realSlug.slice(5, 7);
   const day = realSlug.slice(8, 10);
   const fullPath = join(postsDirectory, `${realSlug}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(fileContents);
 
   const items = {
@@ -23,23 +22,23 @@ export function getPostBySlug(slug, fields = []) {
       year,
       month,
       day,
-      slug: realSlug.slice(11)
-    }
+      slug: realSlug.slice(11),
+    },
   };
 
   // Ensure only the minimal needed data is exposed
-  fields.forEach(field => {
-    if (field === "slug") {
+  fields.forEach((field) => {
+    if (field === 'slug') {
       items[field] = realSlug;
     }
-    if (field === "content") {
+    if (field === 'content') {
       items[field] = content;
     }
-    if (typeof data[field] !== "undefined") {
+    if (typeof data[field] !== 'undefined') {
       // coherce all non-undefined values
       items[field] = data[field];
     }
-    if (field === "date") {
+    if (field === 'date') {
       // coherce date into unix date
       items[field] = data[field].getTime();
     }
@@ -50,7 +49,7 @@ export function getPostBySlug(slug, fields = []) {
 export function getAllPosts(fields = []) {
   const slugs = getPostFilenames();
   const posts = slugs
-    .map(slug => getPostBySlug(slug, fields))
+    .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
