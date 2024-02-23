ef40d5cfeebb49ff81f5a3772f18485d97ea1617
0xADADA <0xadada.pub@0xadada.pub>
2024-02-23 14:47:59 -0500

    added sitemapa

---
+++ src/app/sitemap.ts
@@ -0,0 +1,42 @@
+import { MetadataRoute } from "next";
+import fs from "fs";
+import { join } from "path";
+import { parseFilename } from "../lib/parse-filename";
+
+export function generateStaticParams() {
+  return [{ ["/sitemap.xml"]: "/sitemap.xml" }];
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const docsDir = join(process.cwd(), "docs");
+  const filenames = fs.readdirSync(docsDir);
+  const files = filenames.map((filename) => ({
+    filename: filename,
+    filepath: join(docsDir, filename),
+  }));
+  const unsortedPosts = await Promise.all(
+    files.map(async (file) => {
+      const { date, url } = parseFilename(file.filename);
+      return { date, url };
+    }),
+  );
+  const posts = unsortedPosts.sort((a, b) => (b.date > a.date ? 1 : -1));
+  const sitemap: MetadataRoute.Sitemap = [
+    {
+      url: "https://0xadada.pub/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+  posts.map((post) =>
+    sitemap.push({
+      url: post.url,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    }),
+  );
+
+  return sitemap;
+}
